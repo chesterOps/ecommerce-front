2fb7ed8d90d9adc1fc4ad88eaf2c4348c7430fb9
chester okwulehie <ochester4@gmail.com>
2025-08-27 13:20:52 +0100

    Chester (#18)
    * I don tire ajehhh
    
    * solved an issue on the product detail page
    
    * Worked on product page
    
    * Fixed nav
    
    * More changes
    
    * More changes
    
    * Worked on a lot of stuff
    
    * Fixed issue in cart and wishlist
    
    * Fixed issue on catgeory page
    
    * Fixed issue
    
    * Worked on some stuff
    
    * More changes
    
    * Worked on auth
    
    * Fixed some issues
    
    ---------
    
    Co-authored-by: Sommachi1 <mmesomaf140@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,9 @@ import Order from "./pages/Orders/Order";
 import TrackOrder from "./pages/TrackOrder/TrackOrder";
 import Shop from "./pages/Shop/Shop";
 import Category from "./pages/Category/Category";
-import Search from "./pages/Search/Search";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "./pages/Search/Search";
 
 export default function App() {
   return (
